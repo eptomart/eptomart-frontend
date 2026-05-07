40353ddd5fb9936c6d62f7a2977e3958c6d23a72
Nithyanandam <eptosicare@gmail.com>
2026-05-08 00:29:17 +0530

    config: update contact phone number to +91 9514519518

--- src/utils/businessInfo.js
+++ src/utils/businessInfo.js
@@ -2,7 +2,7 @@ export const BUSINESS = {
   name:    'Eptomart',
   tagline: "India's fast, affordable online shopping destination",
   address: 'No.2, 3rd St, Janaki Nagar, Karthikeyan Nagar, Maduravoyal, Chennai, Tamil Nadu – 600095',
-  phone:   '+91 6369 129 995',
+  phone:   '+91 9514519518',
   email:   'support@eptomart.com',
   website: 'www.eptomart.com',
   state:   'Tamil Nadu',
